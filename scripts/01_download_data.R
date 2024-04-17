f323078871f2e66b77dacc2f40351f9a02d73428
prajogt <113074010+prajogt@users.noreply.github.com>
2024-04-17 14:05:36 -0400

    move data + add data dictionaries

--- scripts/01_download_data.R
+++ scripts/01_download_data.R
@@ -16,16 +16,16 @@ library(openxlsx)
 # Get commercial catch data
 url <- "https://api-proxy.edh.azure.cloud.dfo-mpo.gc.ca/catalogue/records/7ac5fe02-308d-4fff-b805-80194f8ddeb4/attachments/ise-ecs.zip"
 
-download.file(url, "input/salmon-data-bundle.zip", mode = "wb")
+download.file(url, "input/data/salmon-data-bundle.zip", mode = "wb")
 
-zip_file <- "input/salmon-data-bundle.zip"
+zip_file <- "input/data/salmon-data-bundle.zip"
 
-unzip(zip_file, exdir="input/")
+unzip(zip_file, exdir="input/data/")
 
 # Get salmon spawning data
 url <- "https://api-proxy.edh.azure.cloud.dfo-mpo.gc.ca/catalogue/records/c48669a3-045b-400d-b730-48aafe8c5ee6/attachments/All%20Areas_Simplified%20Version_20240110.xlsx"
-download.file(url, "input/nuseds.xlsx", mode = "wb")
+download.file(url, "input/data/nuseds.xlsx", mode = "wb")
 
-nuseds_data_xlsx <- read.xlsx("input/nuseds.xlsx")
+nuseds_data_xlsx <- read.xlsx("input/data/nuseds.xlsx")
 
-write_csv(nuseds_data_xlsx, "input/nuseds.csv")
+write_csv(nuseds_data_xlsx, "input/data/nuseds.csv")
