f1075f1c2b195306bf1aefcd778258642cee11a9
prajogt <113074010+prajogt@users.noreply.github.com>
2024-04-17 14:50:59 -0400

    Use full dataset

--- scripts/01_download_data.R
+++ scripts/01_download_data.R
@@ -23,9 +23,17 @@ zip_file <- "input/data/salmon-data-bundle.zip"
 unzip(zip_file, exdir="input/data/")
 
 # Get salmon spawning data
-url <- "https://api-proxy.edh.azure.cloud.dfo-mpo.gc.ca/catalogue/records/c48669a3-045b-400d-b730-48aafe8c5ee6/attachments/All%20Areas_Simplified%20Version_20240110.xlsx"
+url <- "https://api-proxy.edh.azure.cloud.dfo-mpo.gc.ca/catalogue/records/c48669a3-045b-400d-b730-48aafe8c5ee6/attachments/All%20Areas%20NuSEDS_20240110.xlsx"
 download.file(url, "input/data/nuseds.xlsx", mode = "wb")
 
 nuseds_data_xlsx <- read.xlsx("input/data/nuseds.xlsx")
+
+# Filter only relevant columns, otherwise the file becomes too large to save to github
+nuseds_data_xlsx <- 
+  nuseds_data_xlsx |> 
+  select(-GAZETTED_NAME, -LOCAL_NAME_2, -ENUMERATION_METHODS, 
+         -NATURAL_ADULT_FEMALES, -NATURAL_ADULT_MALES, -EFFECTIVE_FEMALES, -WEIGHTED_PCT_SPAWN,
+         -WATERSHED_CDE, -POPULATION, -ACCURACY, -PRECISION, -INDEX_YN, -RELIABILITY, 
+         -ESTIMATE_STAGE, -ESTIMATE_CLASSIFICATION, -NO_INSPECTIONS_USED, -ESTIMATE_METHOD, -CREATED_DTT, -UPDATED_DTT)
 
 write_csv(nuseds_data_xlsx, "input/data/nuseds.csv")
