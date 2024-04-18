f9ae1b3d831d71de5e26821e29e07473c6fa98c1
prajogt <113074010+prajogt@users.noreply.github.com>
2024-04-17 20:08:20 -0400

    use readxl to properly handle date values

--- scripts/01_download_data.R
+++ scripts/01_download_data.R
@@ -9,7 +9,8 @@
 #### Workplace setup ####
 
 library(tidyverse)
-library(openxlsx)
+library(readxl)
+library(lubridate)
 
 #### Download and save Salmon data ####
 
@@ -26,7 +27,7 @@ unzip(zip_file, exdir="input/data/")
 url <- "https://api-proxy.edh.azure.cloud.dfo-mpo.gc.ca/catalogue/records/c48669a3-045b-400d-b730-48aafe8c5ee6/attachments/All%20Areas%20NuSEDS_20240110.xlsx"
 download.file(url, "input/data/nuseds.xlsx", mode = "wb")
 
-nuseds_data_xlsx <- read.xlsx("input/data/nuseds.xlsx")
+nuseds_data_xlsx <- read_excel("input/data/nuseds.xlsx")
 
 # Filter only relevant columns, otherwise the file becomes too large to save to github
 nuseds_data_xlsx <- 
@@ -35,5 +36,7 @@ nuseds_data_xlsx <-
          -NATURAL_ADULT_FEMALES, -NATURAL_ADULT_MALES, -EFFECTIVE_FEMALES, -WEIGHTED_PCT_SPAWN,
          -WATERSHED_CDE, -POPULATION, -ACCURACY, -PRECISION, -INDEX_YN, -RELIABILITY, 
          -ESTIMATE_STAGE, -ESTIMATE_CLASSIFICATION, -NO_INSPECTIONS_USED, -ESTIMATE_METHOD, -CREATED_DTT, -UPDATED_DTT)
+
+nuseds_data_xlsx$START_DTT <- ymd(nuseds_data_xlsx$START_DTT)
 
 write_csv(nuseds_data_xlsx, "input/data/nuseds.csv")
